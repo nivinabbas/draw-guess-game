730b8dbe4e515829c6688ce40e5aad31e7fcbfdc
nivinabbas <nivin.abbas1@gmail.com>
2022-03-26 21:43:51 +0300

    refactor: add home screen

---
+++ src/components/Start/index.js
@@ -0,0 +1,45 @@
+import './style.css';
+import Welcome from '../Animations/Welcome';
+import Button from '../Button';
+import { useState, useEffect } from 'react';
+
+const Start = ({ history, socket }) => {
+  const roomId = localStorage.getItem('roomId');
+  const [bestScore, setBestScore] = useState(null);
+  const fetchBestScore = async () => {
+    const res = await fetch('/best-score');
+    const json = await res.json();
+    if (res.status === 200) setBestScore(json.bestScore);
+  };
+  useEffect(() => {
+    fetchBestScore();
+  }, []);
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%'
+      }}
+    >
+      <h1 style={{ color: '#6225e6' }}>
+        {!bestScore && bestScore !== 0
+          ? 'Loading best Score....'
+          : `Score : ${bestScore}`}
+      </h1>
+      <div style={{ width: '80%', marginTop: -50 }}>
+        <Welcome />
+      </div>
+      <Button
+        onStart={() => {
+          socket.emit('room-create', { roomId });
+          history.push('/waiting');
+        }}
+      />
+    </div>
+  );
+};
+
+export default Start;
